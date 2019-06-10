b299d54594f956399783343c5e2ef125ec13897c
bh <bwhellar@gmail.com>
2019-06-10 09:54:29 -0700

    changed multipled issues where doctor was not showing. Still issues with env linking

--- src/DoctorLookup.js
+++ src/DoctorLookup.js
@@ -1,16 +1,15 @@
-export class DocDoc {
-  Doctors(search) {
+export class DoctorLookup {
+  lookupDoctor(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let key = process.env.exports.apiKey
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=wa-seattle&user_location=47.676507%2C122.386223&skip=0&limit=10&user_key=` + key
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(Error(request.statusText));
         }
-      };
+      }
       request.open("GET", url, true);
       request.send();
     });
--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
-import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import { DocDoc } from "./DocDoc.js";
+import $ from "jquery";
+import { DoctorLookup } from "./DoctorLookup.js";
 
-$(document).ready(function() {
-  $("#form").submit(function(event) {
+$(document).ready(function(){
+  $("#doctorForm").submit(function(event) {
     event.preventDefault();
-    $("#results").empty();
-
-    let doctor = $('')
+    $("#resultsArea").text("");
+    $("#errorField").text("");
+    let doctorName = $("#doctorSearch").val();
+    let foundDoctor = new DoctorLookup();
+    let promise = foundDoctor.lookupDoctor(doctorName);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      if (body.data.length ===0) {
+        $("#errorField").text("No doctors match this search.");
+      }
+      body.data.forEach(function(doctor) {
+        $("#resultsArea").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br>
+        ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip} <br>
+        Phone number: ${doctor.practices[0].phones[0].number} <br>
+        Accept new patient: ${doctor.practices[0].accepts_new_patients} </li> <br>`)
+      });
+    }, function(error) {
+      $('.showErrors').text(`ERROR: ${error.message}`);
+    });
+  });
+});
